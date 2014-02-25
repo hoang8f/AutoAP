cf058fce6de39cfbd31fb54fc10f6ca3803b5ccf
Le Van Hoang <hoang8f@gmail.com>
2014-02-26 04:35:31 +0900

    Add widget provider

---
+++ AutoAP/src/main/java/info/hoang8f/autoap/widget/WidgetProvider.java
@@ -0,0 +1,16 @@
+package info.hoang8f.autoap.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by hoang8f on 2/26/14.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
