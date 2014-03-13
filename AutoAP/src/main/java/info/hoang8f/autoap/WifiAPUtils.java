bdb906b57be3d085314e35eb3f64415d73dd3d40
TranVu <tranvu217@gmail.com>
2014-03-14 04:17:57 +0900

    Add Network Config

--- AutoAP/src/main/java/info/hoang8f/autoap/WifiAPUtils.java
+++ AutoAP/src/main/java/info/hoang8f/autoap/WifiAPUtils.java
@@ -1,8 +1,10 @@
 package info.hoang8f.autoap;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,18 +14,32 @@ import java.lang.reflect.Method;
  */
 public class WifiAPUtils {
 
+    public static String SECURE_OPEN = "Open";
+    public static String SECURE_WPA = "WPA";
+    public static String SECURE_WPA2 = "WPA2";
+    public static int PASS_MIN_LENGHT = 8;
+
     WifiManager mWifiManager;
     Context context;
+    String ssid = "Auto AP";
+    String securityType = SECURE_OPEN;
+    String password = "12345678";
 
     public WifiAPUtils(Context context) {
         this.context = context;
         mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     public boolean setAP(boolean shouldOpen) {
         WifiConfiguration wifi_configuration = new WifiConfiguration();
-        wifi_configuration.SSID = "AutoAP Access Point";
-        wifi_configuration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        wifi_configuration.SSID = ssid;
+        if (securityType.equals(SECURE_OPEN)) {
+            wifi_configuration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        } else {
+//            wifi_configuration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            wifi_configuration.enterpriseConfig.setPassword(password);
+        }
         mWifiManager.setWifiEnabled(false);
         try {
             //USE REFLECTION TO GET METHOD "SetWifiAPEnabled"
